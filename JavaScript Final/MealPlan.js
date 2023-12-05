2d697edefd3ce65ecd9894c805259e9eba2ede27
svesparza <132864737+svesparza@users.noreply.github.com>
2023-12-04 23:10:37 -0500

    Add files via upload

--- JavaScript Final/MealPlan.js
+++ JavaScript Final/MealPlan.js
@@ -82,36 +82,3 @@ var canvas = document.querySelector('canvas'),
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-
-
-var letters = 'ABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZ';
-letters = letters.split('');
-
-
-var fontSize = 10,
-    columns = canvas.width / fontSize;
-
-
-var drops = [];
-for (var i = 0; i < columns; i++) {
-  drops[i] = 1;
-}
-
-
-function draw() {
-  ctx.fillStyle = 'rgba(0, 0, 0, .1)';
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  for (var i = 0; i < drops.length; i++) {
-    var text = letters[Math.floor(Math.random() * letters.length)];
-    ctx.fillStyle = '#0f0';
-    ctx.fillText(text, i * fontSize, drops[i] * fontSize);
-    drops[i]++;
-    if (drops[i] * fontSize > canvas.height && Math.random() > .95) {
-      drops[i] = 0;
-    }
-  }
-}
-
-setInterval(draw, 33);
-//Matrix Code By Clyde Cooper
-// https://codepen.io/yaclive/pen/EayLYO 
